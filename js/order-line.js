77f42eac76847516894ba1019a2d18702bf5e638
Alexey Demyanov <deal110988@him-mail.ch>
2024-05-15 17:23:26 +0200

    styling the form

--- js/order-line.js
+++ js/order-line.js
@@ -5,11 +5,11 @@ const orderLinesList = formElement.querySelector(".order_lines");
 export const renderOrderLine = () => {
   const orderLineItem = document.createElement("li");
   orderLineItem.innerHTML = `
-  <input type="text">
-  <input type="text">
-  <input type="text">
-  <input type="text">
-  <input type="text">`
+  <input class="order-line--product" type="text">
+  <input class="order-line--supplier-ref" type="text">
+  <input class="order-line--quantity" type="text">
+  <input class="order-line--unit-price" type="text">
+  <input class="order-line--total-price" type="text">`;
 
   orderLinesList.appendChild(orderLineItem);
 };
