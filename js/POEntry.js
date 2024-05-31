6fe129b92439e472a876f3b0335310cdd2abd2db
Alexey Demyanov <deal110988@him-mail.ch>
2024-05-31 12:54:05 +0200

    fixed date rendering on the po entries list items

--- js/POEntry.js
+++ js/POEntry.js
@@ -6,9 +6,17 @@ const poEntriesListElement = bodyElement.querySelector(".po-entries-list");
 export const renderPoEntry = (poEntry) => {
   const poEntryListItem = document.createElement("li");
 
+  const renderDate = (date) => {
+    if (date) {
+      return new Date(date).toLocaleDateString();
+    } else {
+      return "";
+    }
+  };
+
   poEntryListItem.innerHTML = `
   <div class="po-entries-list--number">${poEntry.orderNumber}</div>
-  <div class="po-entries-list--date">${poEntry.date}</div>
+  <div class="po-entries-list--date">${renderDate(poEntry.date)}</div>
   <div class="po-entries-list--user">${poEntry.user}</div>
   <div class="po-entries-list--company">${poEntry.company}</div>
   <div class="po-entries-list--supplier">${poEntry.supplier}</div>`;
